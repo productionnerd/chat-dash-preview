f4d6808bc9e223ddf5508986ee5c21fb3ef9774f
Productionnerd <87790274+productionnerd@users.noreply.github.com>
2023-01-06 20:24:51 -0700

    Add files via upload

---
+++ router.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function Router(routes) {
+    this.routes = routes;
+    const that = this;
+    this.rootElem = document.getElementById('content');
+    this.hasChanged = function() {
+        var r = this.routes;
+        if (window.location.hash.length > 0) {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if (route.isActiveRoute(window.location.hash.substr(1))) {
+                    this.goToRoute(route.htmlName);
+                }
+            }
+        } else {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if (route.default) {
+                    this.goToRoute(route.htmlName);
+                }
+            }
+        }
+    }
+    this.goToRoute = function(htmlName) {
+        var url = htmlName,
+            xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {}
+        };
+        xhttp.open('GET', url + '?t=' + Date.now(), true);
+        xhttp.send();
+    };
+
+    this.addRoutes = function(newRoutes) {
+        newRoutes.forEach(function(newRoute, idx) {
+            let found = false;
+            that.routes.forEach(function(route) {
+                if (route.htmlName == newRoute.htmlName) found = true;
+            });
+            if (!found) that.routes.push(newRoute);
+        });
+    };
+
+    window.addEventListener('hashchange', function(e) {
+        that.hasChanged();
+    });
+    that.hasChanged();
+};
